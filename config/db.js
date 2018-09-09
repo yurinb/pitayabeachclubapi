c89cd24e0e9c8931c197761958620737d5db8e7a
Yuri Bento <yurinbento@hotmail.com>
2018-09-09 17:43:04 -0300

    fixing mongo connection

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const ObjectID = mongodb.ObjectID;
 
 const SLIDER_COLLECTION = "slider";
 
+let db;
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
     if (err) {
@@ -12,9 +13,10 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
     // Save database object from the callback for reuse.
     db = client.db();
     console.log("Database connection ready");
-    
-    module.exports = {
-        db: db,
-        SLIDER_COLLECTION: SLIDER_COLLECTION
-    };
 });
+
+
+module.exports = {
+    db: db,
+    SLIDER_COLLECTION: SLIDER_COLLECTION
+};
