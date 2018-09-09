24aee0e5e926748715cd520d59fc28f4b8e4d96e
Yuri Bento <yurinbento@hotmail.com>
2018-09-09 18:02:45 -0300

    fixing mongo connection

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -14,7 +14,7 @@ server.app.post('/slider', (req, res) => {
     let base64img = Buffer.from(req.files.img.data).toString('base64');
     mongo.db.collection(mongo.SLIDER_COLLECTION).insertOne(base64img, function (err, doc) {
         if (err) {
-            handleError(res, err.message, "Failed to create new contact.");
+            console.log('ERROR: '+err);
         } else {
             res.status(201).json(doc.ops[0]);
         }
@@ -26,7 +26,7 @@ server.app.post('/slider', (req, res) => {
 server.app.get('/slider', (req, res) => {
     mongo.db.collection(mongo.SLIDER_COLLECTION).find({}).toArray(function (err, docs) {
         if (err) {
-            handleError(res, err.message, "Failed to get contacts.");
+            console.log('ERROR: '+err);
         } else {
             res.status(200).json(docs);
         }
