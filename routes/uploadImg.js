a26b89d9a9fb1301814c8af703d9c64dc1d78cc6
Yuri Bento <yurinbento@hotmail.com>
2018-09-09 18:06:03 -0300

    fixing mongo connection

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -1,6 +1,4 @@
 const server = require('../config/server');
-var fs = require('fs');
-
 const mongo = require('../config/db');
 
 mongo.connectClient((client) => {
@@ -8,13 +6,10 @@ mongo.connectClient((client) => {
 });
 
 server.app.post('/slider', (req, res) => {
-    console.log('aaaaaaaaaaaaa');
-    console.log(mongo);
-    console.log('aaaaaaaaaaaaa');
     let base64img = Buffer.from(req.files.img.data).toString('base64');
     mongo.db.collection(mongo.SLIDER_COLLECTION).insertOne(base64img, function (err, doc) {
         if (err) {
-            console.log('ERROR: '+err);
+            //console.log('ERROR: ' + err);
         } else {
             res.status(201).json(doc.ops[0]);
         }
@@ -26,7 +21,7 @@ server.app.post('/slider', (req, res) => {
 server.app.get('/slider', (req, res) => {
     mongo.db.collection(mongo.SLIDER_COLLECTION).find({}).toArray(function (err, docs) {
         if (err) {
-            console.log('ERROR: '+err);
+            console.log('ERROR: ' + err);
         } else {
             res.status(200).json(docs);
         }
