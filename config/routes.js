05d4c8cd4103eeed4581e6f3f740a9bdfdefced4
Yuri Bento <yurinbento@hotmail.com>
2018-09-22 01:41:22 -0300

    refactoring

---
+++ config/routes.js
@@ -0,0 +1,19 @@
+// routes
+require('../routes/converter');
+
+require('../routes/home/slider');
+require('../routes/home/event');
+require('../routes/home/menu');
+require('../routes/home/desc');
+
+require('../routes/apart/banner');
+require('../routes/apart/room');
+require('../routes/apart/galery');
+require('../routes/apart/desc');
+
+require('../routes/garopaba/desc');
+require('../routes/garopaba/slider');
+
+require('../routes/gastro/banner');
+require('../routes/gastro/desc');
+require('../routes/gastro/slider');
--- config/server.js
+++ config/server.js
@@ -1,11 +1,9 @@
-const app = require('express')();
-var bodyParser = require("body-parser");
+const app        = require('express')();
+var   bodyParser = require("body-parser");
 const fileUpload = require('express-fileupload');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
 
 app.use(function (req, res, next) {
@@ -14,22 +12,11 @@ app.use(function (req, res, next) {
     next();
 });
 
+// database
 require('../config/db');
 
-module.exports = {
-    app: app,
-};
-
 // routes
-require('../routes/home/slider');
-require('../routes/home/event');
-require('../routes/home/menu');
-require('../routes/home/desc');
-require('../routes/apart_hotel/banner');
-require('../routes/apart_hotel/room');
-require('../routes/apart_hotel/galery');
-require('../routes/apart_hotel/desc');
-require('../routes/converter');
+require('./routes');
 
 // no sleep
 const http = require("http");
@@ -37,3 +24,8 @@ setInterval(function () {
     console.log('[auto-ping]');
     http.get("http://pitayabeachapi.herokuapp.com");
 }, 50000);
+
+
+module.exports = {
+    app: app,
+};
