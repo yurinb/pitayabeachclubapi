c9a70c31a0a01afc0c14f101668decad57b44226
Yuri Bento <yurinbento@hotmail.com>
2018-10-02 22:08:41 -0300

    allow methods

--- middleware/allow.js
+++ middleware/allow.js
@@ -2,7 +2,7 @@ const SERVER = require('../config/server');
 
 SERVER.app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", req.method);
+    res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
